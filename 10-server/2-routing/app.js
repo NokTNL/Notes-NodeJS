c5c310e5aa8b19aa17cef38370485998361b9940
NokTNL <noklamtsz@gmail.com>
2023-02-03 20:03:30 +0000

    Add async code notes

--- 10-server/2-routing/app.js
+++ 10-server/2-routing/app.js
@@ -2,7 +2,9 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // Write a simple page routing
+  /**
+   * Write a simple page routing
+   */
   if (req.url === "/") {
     res.write("<html>");
     res.write("<head></head>");
@@ -14,10 +16,14 @@ const server = http.createServer((req, res) => {
                </body>
     `);
     res.write("</html>");
-    // `return` here to prevent writing into the response after end()
-    return res.end();
+    // res.end tells Node that the response is considered ready to be sent; any writing to `res` will throw errors
+    res.end();
+    // RETURN here to prevent the code below to be run
+    return;
   }
-  // The form submitted in the above "/" route will have data directed to here
+  /**
+   * The form submitted in the above "/" route will have data directed to here
+   */
   if (req.url === "/message" && req.method === "POST") {
     // NodeJS does not have a `req.body` field, because the data from a request comes in a STREAM, i.e. the request has arrived, but the data has not fully arrived (esp. when data is huge)
     // NodeJS (and Javascript in general) allows you to work with request data BEFORE all data has arrived, by splitting data into CHUNKS
@@ -34,14 +40,16 @@ const server = http.createServer((req, res) => {
       const parsedBody = Buffer.concat(body).toString();
       console.log(parsedBody);
       // Write the message to a file!
-      fs.writeFile("message.txt", parsedBody);
-    });
+      fs.writeFileSync("message.txt", parsedBody);
 
-    // 302 = resource found, redirecting
-    res.statusCode = 302;
-    // the 'Location' header will redirect user to a certain path
-    res.setHeader("Location", "/");
-    return res.end();
+      // Here it end the response AFTER file writing is done
+      // 302 = resource found, redirecting
+      res.statusCode = 302;
+      // the 'Location' header will redirect user to a certain path
+      res.setHeader("Location", "/");
+      res.end();
+    });
+    return;
   }
 
   // No match default route
@@ -51,6 +59,7 @@ const server = http.createServer((req, res) => {
   res.write("<body><h1>Hello World!</h1></body>");
   res.write("</html>");
   res.end();
+  return;
 });
 
 server.listen(3000);
---
+++ 10-server/3-async-code/app.js
@@ -0,0 +1,51 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write("<head></head>");
+    res.write(`<body>
+                  <form action="/message" method="POST">
+                  <input type="text" name="message"/>
+                  <button type="submit">Submit</button>
+                 </form>
+               </body>
+    `);
+    res.write("</html>");
+    res.end();
+    return;
+  }
+  if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      // If we don't want file writing to block request handling, we can outsource it to the OS (the WORKERS, which can call on multiple threads to speed up the process!) and use the ASYNC version of `writeFile`
+      // It requires a CALLBACK as the 3rd argument that will be called after the file writing is finished. The callback receives an error object that will be `null` when no errors are present
+      // Now we can put the response writing code in the callback
+      fs.writeFile("message.txt", parsedBody, (err) => {
+        console.log("File writing done!");
+
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        res.end();
+      });
+    });
+    return;
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head></head>");
+  res.write("<body><h1>Hello World!</h1></body>");
+  res.write("</html>");
+  res.end();
+  return;
+});
+
+server.listen(3000);
